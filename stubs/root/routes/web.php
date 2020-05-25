0d7023bfec9ab63d92fb345472da044283beb5a2
Simon Depelchin <dep.simon@gmail.com>
2020-05-25 09:35:14 +0200

    Add explicit auth routes with namespace

--- stubs/root/routes/web.php
+++ stubs/root/routes/web.php
@@ -1,8 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth;
 use Illuminate\Support\Facades\Route;
 
-Auth::routes();
+Route::get('login', [Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [Auth\LoginController::class, 'login']);
+Route::post('logout', [Auth\LoginController::class, 'logout'])->name('logout');
+
+Route::get('register', [Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [Auth\RegisterController::class, 'register']);
+
+Route::get('password/reset', [Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+
+Route::get('password/confirm', [Auth\ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+Route::post('password/confirm', [Auth\ConfirmPasswordController::class, 'confirm']);
+
+Route::get('email/verify', [Auth\VerificationController::class, 'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [Auth\VerificationController::class, 'verify'])->name('verification.verify');
+Route::post('email/resend', [Auth\VerificationController::class, 'resend'])->name('verification.resend');
 
 Route::view('/', 'welcome')->name('home');
